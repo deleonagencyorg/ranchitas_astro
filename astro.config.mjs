96b6c3cecaf81cb76b744d2e4ef02a6160950f1f
Jose de León <holamundo@deleon.agency>
2026-01-27 13:22:39 -0600

    issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
   integrations: [
@@ -12,4 +13,18 @@ export default defineConfig({
       applyBaseStyles: true,
     }),
   ],
+  vite: {
+    // Ensure JSON imports work in SSR mode
+    ssr: {
+      noExternal: ['@astrojs/*'],
+    },
+    json: {
+      stringify: false,
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  },
 });
